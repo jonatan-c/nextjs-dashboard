8906391dc62b5575143e22c2fd249ce852762f95
jonatan-c <jona.cc.93@gmail.com>
2023-12-10 09:59:31 -0300

    last changes

--- app/page.tsx
+++ app/page.tsx
@@ -17,15 +17,15 @@ export default function Page() {
 
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
-          >            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+          >
+            {' '}
+            <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
           </p>
-          <div
-            className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
-          />
+          <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
